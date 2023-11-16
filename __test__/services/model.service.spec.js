87047a28110f8602ce06b74e8ab3a4dd2a91bb37
Luis Miguel Sanchez Velazquez <lmsv930213@gmail.com>
2023-11-16 14:53:56 +0200

    Clean-up test and improve code

--- __test__/services/model.service.spec.js
+++ __test__/services/model.service.spec.js
@@ -1,14 +1,17 @@
-const { INVALID_MODEL_ERROR } = require('../../lib/constants/messages.constants')
-const { ModelService, SequelizeService } = require('../../lib/services/index')
 const fs = require('fs')
 const path = require('path')
+const { unlink } = require('node:fs/promises');
+const { INVALID_MODEL_ERROR } = require('../../lib/constants/messages.constants')
+const { ModelService, SequelizeService } = require('../../lib/services/index')
+
+const outputFolderPath = path.join(__dirname, '..', '..', 'output-models');
 
 describe('Test all methods from Model Service', () => {
     const argv = {
         path: path.join(__dirname, '..', '..', 'example', 'sequelize-app', 'src', 'models', 'index.js')
     }
     const dbPath = SequelizeService.getSequelizePath(argv)
-    test('Should throw an error if the model is not provided', async () => {
+    test('Should throw an error if the model is not provided', () => {
         expect(() => ModelService.exportModel(null, dbPath))
             .toThrow(INVALID_MODEL_ERROR)
         expect(() => ModelService.exportModel({ model: '' }, dbPath))
@@ -16,43 +19,38 @@ describe('Test all methods from Model Service', () => {
         expect(() => ModelService.exportModel({ m: '' }, dbPath))
             .toThrow(INVALID_MODEL_ERROR)
     })
-    test('Should create the .ts class for the provided model', async () => {
+    test('Should create the .ts class for the provided model', () => {
         ModelService.exportModel({ model: 'Province' }, dbPath)
-        expect(fs.existsSync(path.join(__dirname, '..', '..', 'output-models', 'Province.ts')))
-            .toBe(true)
+        expect(fs.existsSync(path.join(outputFolderPath, 'Province.ts'))).toBe(true)
     })
-    test('Should create the .py class for the provided model', async () => {
+    test('Should create the .py class for the provided model', () => {
         ModelService.exportModel({ model: 'Province', extension: 'py' }, dbPath)
-        expect(fs.existsSync(path.join(__dirname, '..', '..', 'output-models', 'Province.py')))
-            .toBe(true)
+        expect(fs.existsSync(path.join(outputFolderPath, 'Province.py'))).toBe(true)
     })
-    test('Should create the .js class for the provided model', async () => {
+    test('Should create the .js class for the provided model', () => {
         ModelService.exportModel({ model: 'Province', extension: 'js' }, dbPath)
-        expect(fs.existsSync(path.join(__dirname, '..', '..', 'output-models', 'Province.js')))
-            .toBe(true)
+        expect(fs.existsSync(path.join(outputFolderPath, 'Province.js'))).toBe(true)
     })
-    test('Should create the .cs class for the provided model', async () => {
+    test('Should create the .cs class for the provided model', () => {
         ModelService.exportModel({ model: 'Province', extension: 'cs' }, dbPath)
-        expect(fs.existsSync(path.join(__dirname, '..', '..', 'output-models', 'Province.cs')))
-            .toBe(true)
+        expect(fs.existsSync(path.join(outputFolderPath, 'Province.cs'))).toBe(true)
     })
-		test('Should create the .php class for the provided model', async () => {
-			ModelService.exportModel({ model: 'Province', extension: 'php' }, dbPath)
-			expect(fs.existsSync(path.join(__dirname, '..', '..', 'output-models', 'Province.php')))
-				.toBe(true)
-		})
+    test('Should create the .php class for the provided model', () => {
+        ModelService.exportModel({ model: 'Province', extension: 'php' }, dbPath)
+        expect(fs.existsSync(path.join(outputFolderPath, 'Province.php'))).toBe(true)
+    })
     test('Should create a java class for the provided model', async () => {
         ModelService.exportModel({ model: 'Province', extension: 'java' }, dbPath)
-        expect(fs.existsSync(path.join(__dirname, '..', '..', 'output-models', 'Province.java')))
-            .toBe(true)
+        expect(fs.existsSync(path.join(outputFolderPath, 'Province.java'))).toBe(true)
     })
 })
 
+//Removing all test model's files created
 afterAll(() => {
-    fs.unlinkSync(path.join(__dirname, '..', '..', 'output-models', 'Province.ts'))
-    fs.unlinkSync(path.join(__dirname, '..', '..', 'output-models', 'Province.js'))
-    fs.unlinkSync(path.join(__dirname, '..', '..', 'output-models', 'Province.java'))
-    fs.unlinkSync(path.join(__dirname, '..', '..', 'output-models', 'Province.py'))
-    fs.unlinkSync(path.join(__dirname, '..', '..', 'output-models', 'Province.cs'))
-    fs.unlinkSync(path.join(__dirname, '..', '..', 'output-models', 'Province.php'))
+    (async () => {
+        const extensions = ['.ts', '.js', '.java', '.py', '.cs', '.php']
+        for (const extension of extensions) {
+            await unlink(path.join(outputFolderPath, `Province${extension}`))
+        }
+    })()
 })
